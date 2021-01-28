0c6834810603d88564b885844603702f058390c8
181268 <lujieni1017>
2021-01-28 15:39:07 +0800

    代码提交

--- src/main/java/com/lujieni/exception/entity/Student.java
+++ src/main/java/com/lujieni/exception/entity/Student.java
@@ -3,23 +3,25 @@ package com.lujieni.exception.entity;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.*;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Accessors(chain = true)
 /*
-    @NotEmpty 用在集合类上面
-    加了@NotEmpty的String类、Collection、Map、数组，是不能为null或者长度为0的(String Collection Map的isEmpty()方法)
+    @NotEmpty 也可以用在集合类上面
+    加了@NotEmpty的String类、Collection、Map、数组，能检测为null或者长度为0的(String Collection Map的isEmpty()方法),因此
+    不需要和@NotNull搭配使用
 
-    @NotBlank只用于String,不能为null且trim()之后size>0
+    @NotBlank只用于String,字段不能为null且trim()之后size>0,不需要和@NotNull搭配使用
 
     @NotNull:不能为null，但可以为empty,没有Size的约束
 
-    @Length(min = 1, max = 5, message = "name的长度为1到5")可以加在String上但它不校验字符串非空,即使是null
+    @Length(min = 1, max = 5, message = "name的长度为1到5")只能加在String上但它不校验字符串非空,即使是null
     它也不会报错,所以要配合@NotNull or @NotEmpty or @NotBlank一起用
-
 
     @Digits(integer = 2, fraction = 2, message = "abc")
     小数位:最多2位小数  整数位:最多2位整数
@@ -36,13 +38,12 @@ import java.util.Date;
 
  */
 public class Student {
-    @NotNull(message = "年龄不能为空哦")
-    @Digits(integer = 2, fraction = 2, message = "整数位最多2位,小数位最多2位")
-    @DecimalMin(value = "10", message = "最小值为10")
-    private Float age;
-    @NotBlank(message = "名字不能为空哦")
-    @Length(min = 1, max = 5, message = "名字长度为1到5")
-    @Pattern(regexp = "CBM",message = "名字必须是CBM")
+    @NotNull(message = "age不能为空")
+    @Range(min = 1L,max = 130L,message = "age必须介于1-130之间")
+    private Integer age;
+
+    @NotBlank(message = "name不能为空")
+    @Length(max =30,message = "name最大为30个字符")
     private String name;
-    private Date date;
+
 }
